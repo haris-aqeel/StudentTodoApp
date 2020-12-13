be99c0276c1add0ca3245d8aac90af23d7f88b7d
Haris Aqeel <harisaqeel.2001@gmail.com>
2020-12-13 22:34:06 +0500

    add

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config();
+// dotenv.config();
 mongoose.connect(`mongodb+srv://root:${process.env.PASSWORD}@studentdatabase.rjcic.mongodb.net/test`,{
     useCreateIndex: true,
     useFindAndModify: true,
